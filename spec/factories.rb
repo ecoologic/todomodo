c5411ed0bea926fae963cce83ce7eed1b94e7d98
ecoologic <erikecoologic@gmail.com>
2011-08-10 23:47:09 +0100

    set of announcements (past/current/future)

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,6 +4,22 @@ Factory.define :user do |f|
   f.password_confirmation {|u| u.password}
 end
 
-Factory.define :announcement do |f|
-  # TODO
+Factory.define :current_announcement do |f|
+  f.starts_at Date.yesterday
+  f.ends_at Date.tomorrow
+  f.message 'current announcement'
 end
+
+Factory.define :past_announcement do |f|
+  f.starts_at Time.now - 2.days
+  f.ends_at Time.now - 1.day
+  f.message 'past announcement'
+end
+
+Factory.define :future_announcement do |f|
+  f.starts_at Time.now + 1.days
+  f.ends_at Time.now + 2.day
+  f.message 'future announcement'
+end
+
+
