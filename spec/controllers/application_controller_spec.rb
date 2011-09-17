df18f1cdd11c0111ed0b8e7826a9c4e41c362c7d
ecoologic <erikecoologic@gmail.com>
2011-09-17 20:59:30 +0100

    session[:has_just_auth] test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,11 +4,45 @@ describe ApplicationController do
 
   describe 'session[:has_just_auth]' do
 
-    it 'should be true just after login'
+    describe 'when not logged in' do
+      it 'should be nil' do
+        get 'show'
+        @request.session[:has_just_auth].should be nil
+      end
+    end
+    
+    describe 'after login' do
+      before(:each) do
+        login_user
+        get 'show'
+      end
 
-    it 'should be nil when not logged in'
+      it 'should be true the first request' do
+        @request.session[:has_just_auth].should be true
+      end
 
-    it 'should be false from second paged visited on'
+      it 'should be false the second request' do
+        get 'show'
+        @request.session[:has_just_auth].should be false
+      end
+
+      describe 'after logout' do
+        before(:each) do
+          logout_user
+          get 'show'
+        end
+
+        it 'should be nil'
+        # TODO: when you know how to logout...
+        # do
+        #   @request.session[:has_just_auth].should be nil
+        # end
+      end
+
+    end
+
   end
+
+
 
 end
