3e07d51c3565ef2924b47009a6e449cc85548805
ecoologic <erikecoologic@gmail.com>
2011-09-20 23:34:49 +0100

    define user.uniquify_name tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,25 @@ describe User do
     it 'should be unique for all user' do
       @user1 = Factory :user, :name => 'erik'
       @user2 = Factory :user, :name => 'erik'
+
       @user1.to_s.should_not be @user2.to_s
+      @user1.to_s.should_not be_blank
+      @user2.to_s.should_not be_blank
     end
 
   end
+
+  describe 'uniquify_name' do
+
+    describe 'when the name is unique' do
+      it 'should save the model with that name'
+    end
+
+    describe 'when the name is not unique' do
+      it 'should save the model with a unique name (not empty)'
+    end
+
+  end
+
 
 end
