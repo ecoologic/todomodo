f803349ebf782e1e7f5cced35cfdc8ae699761cd
ecoologic <erikecoologic@gmail.com>
2011-09-24 14:50:44 +0100

    postponed

--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -16,15 +16,14 @@ describe 'users/index.html.haml' do
     ])
   end
 
-  it 'renders a list of users' do
-    render
-    
-    assert true
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    # TODO: none of these fields are good for mockup
-    # assert_select 'tr>td', :text => USER_NAME , :count => 2
-    # assert_select 'tr>td', :text => USER_ABOUT, :count => 2
-    # assert_select 'tr>td', :text => USER_NOTE , :count => 2
-  end
+  # TODO: tests views with assign and stubs
+  # NOTE: none of these fields are valid for test
+  # it 'renders a list of users' do
+  #   render
+  #   
+  #   Run the generator again with the --webrat flag if you want to use webrat matchers
+  #   assert_select 'tr>td', :text => USER_NAME , :count => 2
+  #   assert_select 'tr>td', :text => USER_ABOUT, :count => 2
+  #   assert_select 'tr>td', :text => USER_NOTE , :count => 2
+  # end
 end
