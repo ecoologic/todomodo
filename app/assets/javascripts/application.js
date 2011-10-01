312c920b77251e9fdd6ff8b4d6792276c5c9f4f3
ecoologic <erikecoologic@gmail.com>
2011-10-01 22:04:09 +0100

    doc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,8 @@
+// ############################################################################
+// ############### if you haven't yet, read README_FOR_APP ####################
+// ###############   BEFORE to start working on the code   ####################
+// ############################################################################
+
 // This is a manifest file that'll be compiled into including all the files listed below.
 // Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
 // be included in the compiled file accessible from http://example.com/assets/application.js
