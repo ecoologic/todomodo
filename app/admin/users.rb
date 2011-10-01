566fea69b0ec4db4f7da1c811a7193a6c2d3170d
ecoologic <erikecoologic@gmail.com>
2011-10-01 22:51:59 +0100

    admin amend

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,22 +2,30 @@ ActiveAdmin.register User do
 
   index do
     # show images for true/false/nil
-    def self.bool_column(col)
+    def bool_column(col)
       column col, :sortable => col do |user|
-        value = user.send(col.to_s) || 'nil'
-        image_tag "#{value}.png"
+        value = user.send(col) || 'nil'
+        image_tag "#{value}.png", :alt => value, :title => value
       end
     end
 
-    column :id
-    column :name
-    column :email
-    bool_column :is_admin
-    bool_column :is_allowed
-    bool_column :is_active
-    column :current_sign_in_at
-    column :last_sign_in_at
-    column :created_at
+    # show datetime format
+    def datetime_column(col)
+      column col, :sortable => col do |user|
+        value = user.send(col).try(:to_s, :datetime) || 'N/A'
+        image_tag "#{value}.png", :alt => value, :title => value
+      end
+    end
+
+    # show columns
+    column          :id
+    column          :name
+    column          :email
+    bool_column     :is_allowed
+    bool_column     :is_active
+    datetime_column :current_sign_in_at
+    datetime_column :last_sign_in_at
+    datetime_column :created_at
     default_actions
   end
 
