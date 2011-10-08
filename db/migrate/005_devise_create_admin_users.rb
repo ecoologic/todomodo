5a62c5fcfa93ca1bf5096b4608a368a520552333
ecoologic <erikecoologic@gmail.com>
2011-10-08 12:53:08 +0100

    erikecoologic admin

--- db/migrate/005_devise_create_admin_users.rb
+++ db/migrate/005_devise_create_admin_users.rb
@@ -16,7 +16,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
     end
 
     # Create a default user
-    AdminUser.create!(:email => 'erik@todomodo.com', :password => 'ciccipucci', :password_confirmation => 'ciccipucci')
+    AdminUser.create!(:email => 'erikecoologic@gmail.com', :password => 'ciccipucci', :password_confirmation => 'ciccipucci')
 
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
